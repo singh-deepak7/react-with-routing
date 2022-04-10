c7e7d3d7886a926234adb80c43a51ccf471e3ed8
singh-deepak7 <singh-deepak@outlook.com>
2022-04-10 16:18:42 -0500

    added images

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -15,7 +15,7 @@ const DUMMY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      "This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!",
   },
 ];
 
@@ -25,7 +25,7 @@ function AllMeetupsPage() {
       <h1>All Meetups</h1>
       <ul>
         {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
+          return <li key={meetup.id}>{meetup.title} <br/> {meetup.description}</li>;
         })}
       </ul>
     </section>
