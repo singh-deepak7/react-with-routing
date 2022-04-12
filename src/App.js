c57452066cb7233497d4fb94674dd78e084148c4
singh-deepak7 <singh-deepak@outlook.com>
2022-04-12 06:59:19 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <div>
-      <MainNavigation />
+      <Layout>
       <Routes>
         <Route path="/" exact element={<AllMeetupsPage/>} />
         <Route path="/new-meetup" element={<NewMeetupPage/>} />
         <Route path="/favorites" element={<FavoritesPage/>} />
       </Routes>
+      </Layout>
     </div>
   );
 }
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,21 +1,23 @@
+import MeetupList from '../components/meetups/MeetupList';
+
 const DUMMY_DATA = [
   {
-    id: "m1",
-    title: "This is a first meetup",
+    id: 'm1',
+    title: 'This is a first meetup',
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
     description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
   },
   {
-    id: "m2",
-    title: "This is a second meetup",
+    id: 'm2',
+    title: 'This is a second meetup',
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
     description:
-      "This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
   },
 ];
 
@@ -23,11 +25,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title} <br/> {meetup.description}</li>;
-        })}
-      </ul>
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
